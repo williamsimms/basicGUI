f97a8363bd48d4d25df68988f196fa4dca278075
williamsimms <williamsimms193@gmail.com>
2020-08-02 21:06:34 -0400

    added start file functionality

--- app.py
+++ app.py
@@ -3,14 +3,30 @@ from tkinter import filedialog, Text
 import os
 
 root = tk.Tk()
+apps = []
 
 
 def addApp():
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+
     filename = filedialog.askopenfilename(
         initialdir="/",
         title="Select File",
         filetypes=(("executables", "*.exe"), ("all files", "*.*")),
     )
+
+
+apps.append(filename)
+for app in apps:
+    label = tk.Label(frame, text=app, bg="gray")
+    label.pack()
+
+
+def runApps():
+    for app in apps:
+        os.startfile(app)
 
 
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
