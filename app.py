1057e770f1d5360e38ce3f0dae1c66e45ddec0af
williamsimms <williamsimms193@gmail.com>
2020-08-02 21:02:28 -0400

    started gui app

---
+++ app.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+root = tk.Tk()
+
+
+def addApp():
+    filename = filedialog.askopenfilename(
+        initialdir="/",
+        title="Select File",
+        filetypes=(("executables", "*.exe"), ("all files", "*.*")),
+    )
+
+
+canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
+canvas.pack()
+
+frame = tk.Frame(root, bg="white")
+
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+openFile = tk.Button(
+    root, text="Open File", padx=10, pady=5, fg="white", bg="#263d42", command=addApp
+)
+openFile.pack()
+
+runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263d42")
+runApps.pack()
+
+root.mainloop()
+
